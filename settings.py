f27e111241240c2b219fbafcb58f0a7582bcdf8d
cake-wakame <pl46@outlook.jp>
2025-11-23 18:20:28 +0900

    Update settings.py

--- settings.py
+++ settings.py
@@ -17,3 +17,48 @@
 +# ALLOWED_HOSTS を環境変数から設定（カンマ区切り）
 +ALLOWED_HOSTS = os.environ.get('ALLOWED_HOSTS', '*').split(',')
 *** End Patch
+
+*** Begin Patch
+*** Update File: portfolio/settings.py
+@@
+ MIDDLEWARE = [
+-    'django.middleware.security.SecurityMiddleware',
++    'django.middleware.security.SecurityMiddleware',
++    # WhiteNoise を最初の方に入れることで静的ファイルを効率的に配信します
++    'whitenoise.middleware.WhiteNoiseMiddleware',
+     'django.contrib.sessions.middleware.SessionMiddleware',
+     'django.middleware.common.CommonMiddleware',
+     'django.middleware.csrf.CsrfViewMiddleware',
+*** End Patch
+
+*** Begin Patch
+*** Update File: portfolio/settings.py
+@@
+ # Static files (CSS, JavaScript, Images)
+ # https://docs.djangoproject.com/en/4.2/howto/static-files/
+ 
+-STATIC_URL = '/static/'
++STATIC_URL = '/static/'
++# 本番用に collectstatic が吐くディレクトリを指定
++STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
++# WhiteNoise 用に圧縮・キャッシュ可能なストレージを利用
++STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+@@
+-DATABASES = {
+-    'default': {
+-        'ENGINE': 'django.db.backends.sqlite3',
+-        'NAME': BASE_DIR / 'db.sqlite3',
+-    }
+-}
++DATABASES = {
++    'default': {
++        'ENGINE': 'django.db.backends.sqlite3',
++        'NAME': BASE_DIR / 'db.sqlite3',
++    }
++}
++
++# If DATABASE_URL is present (e.g. Render Postgres service), override DATABASES
++DATABASE_URL = os.environ.get('DATABASE_URL')
++if DATABASE_URL:
++    DATABASES['default'] = dj_database_url.parse(DATABASE_URL, conn_max_age=600)
+*** End Patch
